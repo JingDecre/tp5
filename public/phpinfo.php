7b049f576c100e318d72386f97a6a593f12c27aa
JingDecre <l761213825@163.com>
2017-10-12 16:19:36 +0800

    垃圾tp5

---
+++ public/phpinfo.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: decre
+ * Date: 2017/10/12
+ * Time: 11:03
+ */
+phpinfo();
---
+++ public/test.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: decre
+ * Date: 2017/10/12
+ * Time: 11:33
+ */
+
+echo 'hello world!';
+echo "this is a debug test page!";
+$a = 123;
+echo $a;
